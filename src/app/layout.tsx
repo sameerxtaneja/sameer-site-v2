fb26f044d499aa93d2be3c0ea1815a450836822a
Sameer Taneja <sameertaneja@Sameers-MacBook-Air.local>
2025-05-11 03:02:16 +0530

    Added Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +16,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head />
+      <Analytics />
+      <SpeedInsights />
       <body className={inter.className}>{children}</body>
     </html>
   )
